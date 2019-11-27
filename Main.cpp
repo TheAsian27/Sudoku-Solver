8aecd3a4967d315087c786698f078c52b6cd2c25
TheAsian27 <43612540+TheAsian27@users.noreply.github.com>
2019-11-27 11:03:51 -0500

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -2,8 +2,17 @@
 
 using namespace std;
 
-public int main () {
-	cells[9][9] puzzle;
-	
+
+int main () {
+	cells puzzle[9][9];
+	ifstream input("sudoku.in");
+	i
+	for  (int i = 0; i < 9; i ++) {
+		for  (int j = 0; j < 9; j ++) {
+			if () {
+				
+			}
+		}
+	}
 	return 0;
 }
